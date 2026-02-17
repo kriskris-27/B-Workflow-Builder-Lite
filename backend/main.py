59eca3518c91a232bd28c09f766f016b8fa673ca
kris <krishnakumars0101@gmail.com>
2026-02-17 10:57:24 +0530

    fix:added env.example,dependency fix, cors fix

--- backend/main.py
+++ backend/main.py
@@ -43,8 +43,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PUT"],
+    allow_headers=["Content-Type", "Authorization", "X-Requested-With"],
 )
 
 # Templates Data
